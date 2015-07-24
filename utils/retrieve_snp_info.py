e28499d5799398ffe30294d6b419d58e04af8e18
daormar <dortiz@prhlt.upv.es>
2015-07-24 17:06:55 +0000

    First working version.

--- utils/retrieve_snp_info.py
+++ utils/retrieve_snp_info.py
@@ -27,15 +27,27 @@ def main(argv):
 
     # retrieve snp page
     site = wiki.Wiki("http://bots.snpedia.com/api.php")
-#    snp = "rs7412"
     pagehandle = page.Page(site,snp)
     snp_page = pagehandle.getWikiText()
 
     # parse snp page
     wikicode = mwparserfromhell.parse(snp_page)
     templates = wikicode.filter_templates()
-    print(templates[0])
-    print(templates[0].get("rsid").value)
+
+    # retrieve information
+    tags=["rsid","Chromosome","position","GMAF","Assembly","GenomeBuild","dbSNPBuild","geno1","geno2","geno3","StabilizedOrientation","Status","Merged","Gene","Gene_s"]
+    values=[]
+    for t in tags:
+        if templates[0].has_param(t):
+            values.append(str(templates[0].get(t).value).strip("\n"))
+        else:
+            values.append("-")
+
+    # print information
+    entry=tags[0]+": "+values[0]+" ;"
+    for i in range(1,len(tags)):
+        entry=entry+" "+tags[i]+": "+values[i]+" ;"
+    print entry
 
 if __name__ == "__main__":
     main(sys.argv)
